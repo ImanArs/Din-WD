3a1e861be672f05277f345c007ddbaabea5f9aec
ImanArs <imanbaevemaill.ru@gmail.com>
2023-09-11 23:12:00 +0600

    add: disabled btn

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const form = document.getElementById('form');
 const inputName = form.querySelector('#name');
 const inputPhone = form.querySelector('#phone');
 const inputPartner = form.querySelector('#partner');
+const formBtn = form.querySelector('#btn');
 
 inputName.addEventListener('change', (e) => e.target.value)
 inputPhone.addEventListener('change', (e) => e.target.value)
 
-
+console.log(localStorage.getItem('user'));
+if (localStorage.getItem("user") === "true") {
+  formBtn.setAttribute("disabled", "disabled"); 
+  console.log(true);
+  formBtn.innerText = "уже в списке"
+  formBtn.classList.add("disabled-btn");
+} else {
+  console.log("none");
+}
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   let obj = {
     name: `${inputName.value}`,
     phone: `${inputPhone.value}`,
   }
-  console.log(obj);
   fetch('https://64662253228bd07b355c9c1c.mockapi.io/din', {
     method: 'POST',
     headers: {
@@ -27,4 +35,9 @@ form.addEventListener('submit', (e) => {
   }, 2000)
   inputName.value = ''
   inputPhone.value = ''
+
+
+  localStorage.setItem("user", true)
+
 })
+
